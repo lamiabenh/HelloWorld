519ee44c4edb481c71a7fc617d55be3e6b558127
Lamia Ben <benhiba.lamia@gmail.com>
2014-09-14 14:42:56 +0000

    test du 14-09-2014

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
 var server = require("./server");
 var router = require("./router");
+var requestHandlers = require("./requestHandlers");
 
-server.start(router.route);
+console.info(require);
+
+var handle =  {};
+handle["/"] = requestHandlers.start; //the start function
+handle["/start"] = requestHandlers.start;
+handle["/upload"] = requestHandlers.upload;
+
+server.start(router.route, handle);
--- server.js
+++ server.js
@@ -1,19 +1,28 @@
 var http = require("http");
 var url = require("url");
 
-function start(route){
+function start(route, handle){
     
     function OnRequest(request,response){
+        var postData = "";
         var pathname = url.parse(request.url).pathname;
         console.log("request for " + pathname + " received");
         
-        route(pathname);
+        request.setEncoding("utf8");
         
-        response.writeHead(200,{"Content-Type":"text/plain"});
-        response.write("Hello World!");
-        response.end();
-    }
+        request.addListener("data", function(postDataChunk){
+            postData += postDataChunk;
+            console.log("POST data chunk received" + postDataChunk);
+        }); 
+        
+        request.addListener("end", function() { 
+            route(handle, pathname, response, postData);
+        });
 
+        //response.writeHead(200,{"Content-Type":"text/plain"});
+        //response.write("Hello World!");
+        //response.end();
+    }
 
     http.createServer(OnRequest).listen(process.env.PORT);
     console.log("Server has started.");
